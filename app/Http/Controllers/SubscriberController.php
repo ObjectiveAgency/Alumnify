0c195509036e73041f9029d15635568d7e7ab273
John <John@Objective.agency>
2016-10-03 20:13:33 +0800

    Controller Constractor

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -8,35 +8,37 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 
 
-<<<<<<< HEAD
-class SubscriberController extends Controller
-{	
-	// public function __construct(test $test){
-	// 	$this->test = $test;
-	// }
-    public function index(test $test){
-    	dd($test->getData('campaigns'));
-    	$subs = \App\subscribers::all();
-        return view('subscribers.index',['subs'=>$subs]);
-=======
 
 class SubscriberController extends Controller
 {
+	// public function __construct(test $test){
+	//  	$this->test = $test;
+	//  }
+	public function index(Apiwrap $test){
+    	dd($test->getData('campaigns'));
+    	$subs = \App\subscribers::all();
+        return view('subscribers.index',['subs'=>$subs]);
 
-    public function index(ApiWrapper $wrapper){
+		}
+}
 
-    	// $dd = $wrapper->getData('lists');
-    	// $subs = \App\subscribers::all();
-        // return view('subscribers.index',['subs'=>$subs]);
-        dd($wrapper->getData('lists'));
->>>>>>> 76b3894d4b0e70a284782185f926f89dc12a791e
+// class SubscriberController extends Controller
+// {
 
-    }
+//     public function index(ApiWrapper $wrapper){
 
-    public function store(Request $request){
+//     	// $dd = $wrapper->getData('lists');
+//     	// $subs = \App\subscribers::all();
+//         // return view('subscribers.index',['subs'=>$subs]);
+//         dd($wrapper->getData('lists'));
+
+
+//     }
+
+//     public function store(Request $request){
 	    
 	
-	    // Create The Task...
-	}
+// 	    // Create The Task...
+// 	}
 
-}
+// }
